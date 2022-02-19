62591c292046bed735f43f5254eaa411f6dbef00
AliA41 <abdelghany.ali@vodafone.com>
2022-02-20 00:53:26 +0200

    fix test

--- src/test/java/com/pinguin/sprintplanning/SprintPlanningApplicationTests.java
+++ src/test/java/com/pinguin/sprintplanning/SprintPlanningApplicationTests.java
@@ -3,7 +3,7 @@ package com.pinguin.sprintplanning;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {SprintPlanningApplication.class})
 class SprintPlanningApplicationTests {
 
 	@Test
